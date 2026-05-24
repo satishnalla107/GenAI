4552c5fe082d19492e6dd4c82831df5dfb0ef668
Satish Nalla <satishnalla107@gmail.com>
2026-05-24 23:13:51 +0530

    in prgoess chatbot

--- apps/1_qa_chatbot.py
+++ apps/1_qa_chatbot.py
@@ -2,10 +2,14 @@ from dotenv import load_dotenv
 load_dotenv()
 from langchain_google_genai import ChatGoogleGenerativeAI
 llm = ChatGoogleGenerativeAI(model = "gemini-2.5-flash")
+import streamlit as st
 
-while True :
-    query =  (input("user:"))
-    if query.lower() == "exit":
-        break
-    res = llm.invoke(query)
-    print("ai:"+res.content)
+st.title("🤖 Askbuddy - AI QnA Bot")
+st.markdown("Welcome to my QnA bot with Langchain and google gemini !")
+
+query = st.chat_input("Ask - Anything")
+
+if query:
+    st.chat_message("user").markdown(query)
+    res= llm.invoke(query)
+    st.chat_message("ai").markdown(res.content)
