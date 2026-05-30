1bbc9b8cfa178a62819c1be2ddb767bdc600ab83
Satish Nalla <satishnalla107@gmail.com>
2026-05-31 00:00:35 +0530

    made up to session state

--- apps/1_qa_chatbot.py
+++ apps/1_qa_chatbot.py
@@ -7,9 +7,22 @@ import streamlit as st
 st.title("🤖 Askbuddy - AI QnA Bot")
 st.markdown("Welcome to my QnA bot with Langchain and google gemini !")
 
+if "messages" not in st.session_state:
+    st.session_state.messages=[]
+
+for message in st.session_state.messages:
+    role = message.get("role")  
+    content = message.get("content") 
+    st.chat_message(role).markdown(content)
+    
+
+
+
 query = st.chat_input("Ask - Anything")
 
 if query:
+    st.session_state.messages.append({"role":"user","content":query})
     st.chat_message("user").markdown(query)
     res= llm.invoke(query)
     st.chat_message("ai").markdown(res.content)
+    st.session_state.messages.append({"role":"ai","content":res.content})
