129293f41ea0bda28a15510076a2e209acfbbaf5
Satish Nalla <satishnalla107@gmail.com>
2026-06-23 22:42:56 +0530

    sql agent

---
+++ apps/4_sql_agentstreamlit.py
@@ -0,0 +1,87 @@
+from dotenv import load_dotenv
+load_dotenv()
+from langchain_groq import ChatGroq
+from langchain.tools import tool
+from langchain_community.utilities import SQLDatabase
+from langchain_community.agent_toolkits import SQLDatabaseToolkit
+from langgraph.checkpoint.memory import InMemorySaver
+from langchain.agents import create_agent 
+import streamlit as st
+
+
+
+
+db= SQLDatabase.from_uri("sqlite:///mytasks.db")
+
+db.run("""
+       CREATE TABLE IF NOT EXISTS tasks (
+       id INTEGER PRIMARY KEY AUTOINCREMENT,
+       title TEXT NOT NULL,
+       description TEXT,
+       status TEXT CHECK(status IN ('pending','inprogress','completed')) DEFAULT 'pending' ,
+       created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP)
+       """)
+
+### requirements llm,tools,system_prompt,memory
+model = ChatGroq(model="openai/gpt-oss-20b")
+toolkit = SQLDatabaseToolkit(db=db,llm=model)
+tools = toolkit.get_tools()
+memory=InMemorySaver()
+
+system_prompt = """
+You are a task management assistant that interacts with a SQL database containing a 'tasks' table. 
+
+TASK RULES:
+1. Limit SELECT queries to 10 results max with ORDER BY created_at DESC
+2. After CREATE/UPDATE/DELETE, confirm with SELECT query
+3. If the user requests a list of tasks, present the output in a structured table format to ensure a clean and organized display in the browser."
+
+CRUD OPERATIONS:
+    CREATE: INSERT INTO tasks(title, description, status)
+    READ: SELECT * FROM tasks WHERE ... LIMIT 10
+    UPDATE: UPDATE tasks SET status=? WHERE id=? OR title=?
+    DELETE: DELETE FROM tasks WHERE id=? OR title=?
+
+Table schema: id, title, description, status(pending/in_progress/completed), created_at.
+"""
+model = ChatGroq(model="openai/gpt-oss-20b",streaming=True)
+
+@st.cache_resource
+def getagent():
+    agent = create_agent(
+    model = model,
+    tools = tools,
+    checkpointer=  memory,
+    system_prompt= system_prompt)
+    return agent
+
+agent = getagent()
+
+st.subheader("📋 Taskbot - Manage your Tasks")
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+for message in st.session_state.messages:
+    role = message.get("role")  
+    content = message.get("content") 
+    st.chat_message(message["role"]).markdown(message["content"])
+
+
+prompt=st.chat_input("Ask me to manage your tasks?")
+ 
+if prompt:
+    st.chat_message("user").markdown(prompt)
+    st.session_state.messages.append({"role":"user","content":prompt})
+
+    with st.chat_message("assistant"):
+        with st.spinner("Processing....."):
+            res = agent.invoke({"messages":[{"role":"user","content":prompt}]},
+                       {"configurable":{"thread_id":"1"}})
+            result= res["messages"][-1].content
+            st.markdown(result) 
+            st.session_state.messages.append({"role":"assistant","content":result})
+    
+
+    
+
+
